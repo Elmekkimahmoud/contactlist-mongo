1563dfe747d0985de3756f35391ea9be6a9a508a
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-03-10 15:39:02 +0100

    Updates

---
+++ precache-manifest.f5e7296c3eb187055655b95bf4d2cf88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef6a37c11411c5c6d9162becbf778e1c",
+    "url": "/contactlist-mongo/index.html"
+  },
+  {
+    "revision": "53a7b3093c602dab8fa0",
+    "url": "/contactlist-mongo/static/css/main.29474ae3.chunk.css"
+  },
+  {
+    "revision": "9a1a90d47abcc1876ac1",
+    "url": "/contactlist-mongo/static/js/2.10af9c49.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/contactlist-mongo/static/js/2.10af9c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a7b3093c602dab8fa0",
+    "url": "/contactlist-mongo/static/js/main.602e1e6f.chunk.js"
+  },
+  {
+    "revision": "c9ad649c844f2bc5d708",
+    "url": "/contactlist-mongo/static/js/runtime-main.a6aab6fd.js"
+  }
+]);
